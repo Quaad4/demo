e4d68f97d820a1994dc55776b93e7b28747bcb54
Alex Wood <ajwood1998@hotmail.co.uk>
2025-02-25 14:00:41 +0100

    added validation on the server side in the controller to check if a note has a body or whether the character amount exceeds 1000 and returning an error message. Also redisplaying the user's input in the textarea if there is an error message so the user does not have to retype their message again

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -6,11 +6,22 @@ $config = require('config.php');
 $db = new Database($config['database']);
 
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $errors = [];
 
     $body = $_POST['body'];
-    $query = "INSERT INTO notes (body, user_id) VALUES (:body, :user_id)";
+    if(!strlen(trim($body))) {
+        $errors['body'] = 'A body is required';
+    }
 
-    $db->query($query, ['body' => $body, 'user_id' => 1]);
+    if(strlen(trim($body)) > 1000) {
+        $errors['body'] = 'A body can not be more than 1,000 characters.';
+    }
+
+    if(empty($errors)) {
+        $query = "INSERT INTO notes (body, user_id) VALUES (:body, :user_id)";
+
+        $db->query($query, ['body' => $body, 'user_id' => 1]);
+    }
 }
 
 require 'views/note-create.view.php';
