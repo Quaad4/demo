a7850a129db087b114e5f399c969dd12358aa93a
Alex Wood <ajwood1998@hotmail.co.uk>
2025-02-27 12:45:49 +0100

    moved router and functions into core folder

---
+++ core/functions.php
@@ -0,0 +1,31 @@
+<?php
+
+use core\Response;
+
+function dd($value) {
+    echo "<pre>";
+    var_dump($value);
+    echo "</pre>";
+
+    die();
+}
+
+function UrlIs($value) {
+    return $_SERVER['REQUEST_URI'] === $value;
+}
+
+function authorize($condition, $status = Response::FORBIDDEN) {
+    if(!$condition) {
+        return abort($status);
+    }
+}
+
+function base_path($path) {
+    return BASE_PATH . $path;
+}
+
+function view($path, $attributes = []) {
+    extract($attributes);
+    
+    require base_path('views/' . $path);
+}
